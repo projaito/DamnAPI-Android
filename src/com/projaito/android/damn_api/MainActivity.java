b437628e0915f220154be053eb015c0910bdd20e
pinglamb <pinglambs@gmail.com>
2011-11-06 03:29:23 +0800

    Display api group

--- src/com/projaito/android/damn_api/MainActivity.java
+++ src/com/projaito/android/damn_api/MainActivity.java
@@ -13,6 +13,7 @@ import java.net.URL;
 import javax.net.ssl.HttpsURLConnection;
 import java.net.MalformedURLException;
 import org.apache.commons.io.IOUtils;
+import org.json.*;
 
 import com.projaito.android.damn_api.widget.*;
 
@@ -21,39 +22,38 @@ public class MainActivity extends Activity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
 
-    // try {
-    //   URL url = new URL("https://raw.github.com/gist/1338795/faa1f0056de928ce372b8db59c335523ef507a86/twitter_api.json");
-    //   new DownloadSpecTask().execute(url);
-    // } catch(MalformedURLException e) {
-    //   e.printStackTrace();
-    // }
-
-    AmazingListView list = (AmazingListView)findViewById(R.id.list);
-    list.setPinnedHeaderView(LayoutInflater.from(this).inflate(R.layout.list_header, list, false));
-    list.setAdapter(new ApiGroupAdapter(this));
+    try {
+      URL url = new URL("https://raw.github.com/gist/1338795/faa1f0056de928ce372b8db59c335523ef507a86/twitter_api.json");
+      new DownloadSpecTask().execute(url);
+    } catch(MalformedURLException e) {
+      e.printStackTrace();
+    }
   }
 
-  // private class DownloadSpecTask extends AsyncTask<URL, Void, String> {
-  //   protected String doInBackground(URL... urls) {
-  //     URL url = urls[0];
-  //     HttpsURLConnection urlConnection = null;
-  //     String result = null;
-  //     try {
-  //       urlConnection = (HttpsURLConnection) url.openConnection();
-  //       InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-  //       result = IOUtils.toString(in, urlConnection.getContentEncoding());
-  //     } catch(IOException e) {
-  //       e.printStackTrace();
-  //     } finally {
-  //       urlConnection.disconnect();
-  //     }
-  //     return result;
-  //   }
+  private class DownloadSpecTask extends AsyncTask<URL, Void, JSONObject> {
+    protected JSONObject doInBackground(URL... urls) {
+      URL url = urls[0];
+      HttpsURLConnection urlConnection = null;
+      JSONObject result = null;
+      try {
+        urlConnection = (HttpsURLConnection) url.openConnection();
+        InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+        String jsonString = IOUtils.toString(in, urlConnection.getContentEncoding());
+        result = new JSONObject(jsonString);
+      } catch(IOException e) {
+        e.printStackTrace();
+      } catch(JSONException e) {
+        e.printStackTrace();
+      } finally {
+        urlConnection.disconnect();
+      }
+      return result;
+    }
 
-  //   protected void onPostExecute(String result) {
-  //     if(result == null) {
-  //       result = "Error";
-  //     }
-  //   }
-  // }
+    protected void onPostExecute(JSONObject result) {
+      AmazingListView list = (AmazingListView)findViewById(R.id.list);
+      list.setPinnedHeaderView(LayoutInflater.from(MainActivity.this).inflate(R.layout.list_header, list, false));
+      list.setAdapter(new ApiSpecAdapter(MainActivity.this, result));
+    }
+  }
 }
---
+++ src/com/projaito/android/damn_api/widget/ApiSpecAdapter.java
@@ -0,0 +1,126 @@
+package com.projaito.android.damn_api.widget;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.util.Pair;
+import org.json.*;
+import java.util.List;
+import java.util.ArrayList;
+
+import com.projaito.android.damn_api.R;
+
+public class ApiSpecAdapter extends AmazingAdapter {
+  private static final String TAG = "ApiSpecAdapter";
+
+  private Context mContext = null;
+  private JSONObject mSpec = null;
+  private List<Pair<String, List<String>>> data = null;
+
+  public ApiSpecAdapter(Context context, JSONObject spec) {
+    mContext = context;
+    mSpec = spec;
+
+    data = new ArrayList<Pair<String, List<String>>>();
+    try {
+      JSONArray apiGroups = mSpec.getJSONArray("api_groups");
+      for(int i=0; i < apiGroups.length(); i++) {
+        JSONObject apiGroup = apiGroups.getJSONObject(i);
+        JSONArray apis = apiGroup.getJSONArray("apis");
+        List apiNames = new ArrayList<String>();
+        for(int j=0; j < apis.length(); j++) {
+          JSONObject api = apis.getJSONObject(j);
+          apiNames.add(api.getString("name"));
+        }
+        data.add(new Pair<String, List<String>>(apiGroup.getString("name"), apiNames));
+      }
+    } catch(JSONException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Override public int getCount() {
+    int res = 0;
+    for (int i = 0; i < data.size(); i++) {
+      res += data.get(i).second.size();
+    }
+    return res;
+  }
+
+  @Override public String getItem(int position) {
+    int c = 0;
+    for (int i = 0; i < data.size(); i++) {
+      if (position >= c && position < c + data.get(i).second.size()) {
+        return data.get(i).second.get(position - c);
+      }
+      c += data.get(i).second.size();
+    }
+    return null;
+  }
+
+  @Override public long getItemId(int position) {
+    return position;
+  }
+
+  @Override protected void bindSectionHeader(View view, int position, boolean displaySectionHeader) {
+    if(displaySectionHeader) {
+      TextView header = (TextView)view.findViewById(R.id.header);
+      header.setVisibility(View.VISIBLE);
+      header.setText(getSections()[getSectionForPosition(position)]);
+    } else {
+      view.findViewById(R.id.header).setVisibility(View.GONE);
+    }
+  }
+
+  @Override public View getAmazingView(int position, View convertView, ViewGroup parent) {
+    View res = convertView;
+    if (res == null) {
+      res = LayoutInflater.from(mContext).inflate(R.layout.list_item, null);
+    }
+
+    TextView name = (TextView)res.findViewById(R.id.name);
+    name.setText(getItem(position));
+    return res;
+  }
+
+  @Override public void configurePinnedHeader(View header, int position, int alpha) {
+    ((TextView)header).setText(getSections()[getSectionForPosition(position)]);
+  }
+
+  @Override public int getPositionForSection(int section) {
+    if (section < 0) section = 0;
+    if (section >= data.size()) section = data.size() - 1;
+    int c = 0;
+    for (int i = 0; i < data.size(); i++) {
+      if (section == i) {
+        return c;
+      }
+      c += data.get(i).second.size();
+    }
+    return 0;
+  }
+
+  @Override public int getSectionForPosition(int position) {
+    int c = 0;
+    for (int i = 0; i < data.size(); i++) {
+      if (position >= c && position < c + data.get(i).second.size()) {
+        return i;
+      }
+      c += data.get(i).second.size();
+    }
+    return -1;
+  }
+
+  @Override public String[] getSections() {
+    String[] res = new String[data.size()];
+    for (int i = 0; i < data.size(); i++) {
+      res[i] = data.get(i).first;
+    }
+    return res;
+  }
+
+  @Override protected void onNextPageRequested(int page) {
+  }
+}
